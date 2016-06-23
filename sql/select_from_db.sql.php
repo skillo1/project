bc48d9a0e4f7d5e6e5c297f1893b73fc3fca1cee
Sorin <sorin.pavel1@gmail.com>
2016-06-23 06:23:13 +0300

    adding stuff

---
+++ sql/select_from_db.sql.php
@@ -0,0 +1,11 @@
+<?php
+include "db.sql.php";
+
+$sql = "SELECT * FROM posts";
+$result  = $conn->query($sql);
+
+while ($row = $result->fetch_assoc()) {
+	echo '<p>' . $row['subject'] . "</p>";
+}
+
+?>
